e91bdd9d697c35420211d1a7d382e32070f85975
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-09-08 23:37:06 +0200

    big update

--- extension/content.js
+++ extension/content.js
@@ -1,23 +1,31 @@
 const myButton = document.getElementById('myButton');
 const statusIndicator = document.getElementById('statusPic');
-const cb = 'Test';
+const cpInput = document.getElementById('textInput');
 
 myButton.addEventListener('click', function () {
-  // Toggle between green and red on each click
+  // Toggle between green and red
   if (statusIndicator.src.includes('red.png')) {
+    if (cpInput.value) {
+      // Copy the text from the input field to the clipboard
+      navigator.clipboard.writeText(cpInput.value)
+        .then(() => {
+          // Display the copied text - alert
+          const cp = cpInput.value;
+          alert(cp);
 
-    //navigator.clipboard.readText();
-
-    //alert(cb)
-//
-    navigator.clipboard.writeText("END OUTPUT TEST");
-
-//green
-    statusIndicator.src = 'img/green.png';
-    console.log('Button clicked (green)');
+          //green
+          statusIndicator.src = 'img/green.png';
+          console.log('Button clicked (green)');
+        })
+        .catch((error) => {
+          console.error('Error copying text to clipboard:', error);
+        });
+    } else {
+      // If the input field is empty
+      console.error('Input field is empty');
+    }
   } else {
-
-//red
+    //red
     statusIndicator.src = 'img/red.png';
     console.log('Button clicked (red)');
   }
