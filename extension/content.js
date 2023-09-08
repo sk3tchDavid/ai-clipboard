faa25c37e5f9b8905ff3763a53aa11024f9e92b1
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-09-08 23:02:01 +0200

    update

--- extension/content.js
+++ extension/content.js
@@ -1,12 +1,23 @@
 const myButton = document.getElementById('myButton');
 const statusIndicator = document.getElementById('statusPic');
+const cb = 'Test';
 
 myButton.addEventListener('click', function () {
   // Toggle between green and red on each click
   if (statusIndicator.src.includes('red.png')) {
+
+    //navigator.clipboard.readText();
+
+    //alert(cb)
+//
+    navigator.clipboard.writeText("END OUTPUT TEST");
+
+//green
     statusIndicator.src = 'img/green.png';
     console.log('Button clicked (green)');
   } else {
+
+//red
     statusIndicator.src = 'img/red.png';
     console.log('Button clicked (red)');
   }
