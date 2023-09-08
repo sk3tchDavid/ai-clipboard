b168e133444e0bbc1cc2f2e2549ccf152a88b6f3
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-09-08 16:36:59 +0200

    update

--- extension/background.js
+++ extension/background.js
@@ -1,43 +1,29 @@
-let isMonitoring = false; // Initialize monitoring state
-
-function startMonitoring() {
-  // Set up a listener to monitor clipboard changes
-  chrome.clipboard.onClipboardDataChanged.addListener(() => {
-    if (isMonitoring) {
-      // You can add your logic here to process the clipboard data
-      // For example, you can access the clipboard data using chrome.clipboard.getData()
-      // and perform actions based on the content.
+chrome.runtime.onInstalled.addListener(() => {
+    // Set up a listener to monitor clipboard changes
+    chrome.clipboard.onClipboardDataChanged.addListener(() => {
+      // logic here to process the clipboard data
+      // access: chrome.clipboard.getData()
       chrome.clipboard.getData({}, (clipboardData) => {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError);
           return;
         }
-
+  
         // Process the clipboard data here
         console.log('Clipboard data:', clipboardData);
-
+  
         // Perform actions based on clipboard content
         // For example, you can send data to a server or modify it in some way.
       });
-    }
+    });
   });
-}
+  
+  //
+//  Digitalisierung bezieht sich auf die Umwandlung analoger Informationen in digitale Formate.
 
-chrome.runtime.onInstalled.addListener(() => {
-  // Set up a listener for changes to the switch state
-  chrome.storage.sync.get(['isMonitoring'], (result) => {
-    isMonitoring = result.isMonitoring || false;
-    if (isMonitoring) {
-      startMonitoring();
-    }
-  });
-});
+//  Datensicherheit schützt Daten vor unbefugtem Zugriff, während Datenschutz die Privatsphäre personenbezogener Daten gewährleistet.
 
-chrome.storage.onChanged.addListener((changes) => {
-  if (changes.isMonitoring) {
-    isMonitoring = changes.isMonitoring.newValue || false;
-    if (isMonitoring) {
-      startMonitoring();
-    }
-  }
-});
+//  Personenbezogene Daten sind Informationen, die auf identifizierbare Personen verweisen, wie Name oder Adresse.
+
+//  Schutz personenbezogener Daten erfordert starke Passwörter, 2FA, Verschlüsselung und Wissen über Datenschutzgesetze.
+  //
